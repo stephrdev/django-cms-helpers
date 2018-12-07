9c394f6a8c052bbd55459044ab460b941b7db719
Magdalena Rother <magdalena.rother@moccu.com>
2018-12-07 11:19:53 +0100

    Port cms helpers code from barbeque

---
+++ cms_helpers/anylink_extension.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import
+
+from anylink.extensions import BaseLink
+from cms.models.fields import PageField
+from cms.utils.conf import get_cms_setting
+from django.conf import settings
+from django.core.cache import cache
+from django.db import models
+from django.utils.six.moves.urllib import parse as urlparse
+from django.utils.translation import get_language
+from django.utils.translation import ugettext_lazy as _
+
+
+class CmsPageLink(BaseLink):
+    name = 'page'
+    verbose_name = _('CMS Page')
+    provided_fields = ('page', 'anchor')
+
+    def configure_model(self, model):
+        model.add_to_class(
+            self.get_name(), PageField(blank=True, null=True))
+        model.add_to_class(
+            'anchor', models.CharField(max_length=256, null=True, blank=True))
+
+    def get_absolute_url(self, link):
+        cache_key = '{0}anylink-page-url:{1}:{2}'.format(
+            get_cms_setting('CACHE_PREFIX'), str(link.page_id), get_language())
+
+        url = cache.get(cache_key)
+        if url is None:
+            if settings.SITE_ID != link.page.site_id:
+                url = '//{domain}{url}'.format(
+                    domain=link.page.site.domain,
+                    url=link.page.get_absolute_url()
+                )
+            else:
+                url = link.page.get_absolute_url()
+
+            cache.set(cache_key, url, get_cms_setting('CACHE_DURATIONS')['content'])
+
+        if link.anchor:
+            parsed = urlparse.urlparse(url)
+            parsed = parsed._replace(fragment=link.anchor)
+            url = parsed.geturl()
+
+        return url
---
+++ cms_helpers/templatetags/page_titleextension.py
@@ -0,0 +1,30 @@
+from cms.models import Page
+from cms.utils.moderator import use_draft
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def page_titleextension(context, page_id, extension):
+    try:
+        page = Page.objects.get(pk=page_id)
+        if 'request' in context and use_draft(context['request']):
+            page = page.get_draft_object()
+        else:
+            page = page.get_public_object()
+    except NameError:
+        raise ImportError(
+            'django-cms is required when using page_titleextension tag')
+    except Page.DoesNotExist:
+        return None
+
+    if not page:
+        return None
+
+    try:
+        return getattr(page.get_title_obj(), extension)
+    except ObjectDoesNotExist:
+        return None
